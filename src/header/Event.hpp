dcb32ed5e06dc56fddf96c2f769d25ec37a3d71a
Nicholas Wise <nicholasaragih@gmail.com>
2025-12-20 14:44:54 +0700

    feat: add starting queue simulator

---
+++ src/header/Event.hpp
@@ -0,0 +1,16 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+enum EventType {ARRIVAL, DEPARTURE};
+
+struct Event {
+  double time;
+  EventType type;
+
+  bool operator>(const Event& other) const {
+    return time > other.time;
+  }
+};
+
+
+#endif
